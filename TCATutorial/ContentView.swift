2c3acfd76447d86a78faeb3985771afdb1a3a1bb
syogo-user <s.photo.trumpet.005@gmail.com>
2024-12-29 08:56:54 +0900

    Initial Commit

---
+++ TCATutorial/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TCATutorial
+//
+//  Created by 小野寺祥吾 on 2024/12/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TCATutorial/TCATutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCATutorialApp.swift
+//  TCATutorial
+//
+//  Created by 小野寺祥吾 on 2024/12/29.
+//
+
+import SwiftUI
+
+@main
+struct TCATutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
