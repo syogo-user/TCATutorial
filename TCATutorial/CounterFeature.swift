46a3618d3bde19b8071f633751d1f3cb29392c4d
syogo-user <s.photo.trumpet.005@gmail.com>
2024-12-30 18:37:21 +0900

    feat: タイマー機能を追加

--- TCATutorial/CounterFeature.swift
+++ TCATutorial/CounterFeature.swift
@@ -16,6 +16,7 @@ struct CounterFeature {
         var count = 0
         var fact: String?
         var isLoading = false
+        var isTimerRunning = false
     }
     
     enum Action {
@@ -23,7 +24,11 @@ struct CounterFeature {
         case factButtonTapped
         case factResponse(String)
         case incrementButtonTapped
+        case timerTick
+        case toggleTimerButtonTapped
     }
+    
+    enum CancelID { case timer }
     
     var body: some ReducerOf<Self> {
         Reduce { state, action in
@@ -53,6 +58,23 @@ struct CounterFeature {
                 state.count += 1
                 state.fact = nil
                 return .none
+            case .timerTick:
+                state.count += 1
+                state.fact = nil
+                return .none
+            case .toggleTimerButtonTapped:
+                state.isTimerRunning.toggle()
+                if state.isTimerRunning {
+                    return .run { send in
+                        while true {
+                            try await Task.sleep(for: .seconds(1))
+                            await send(.timerTick)
+                        }
+                    }
+                    .cancellable(id: CancelID.timer)
+                } else {
+                    return .cancel(id: CancelID.timer)
+                }
             }
             
         }
--- TCATutorial/CounterView.swift
+++ TCATutorial/CounterView.swift
@@ -34,23 +34,32 @@ struct CounterView: View {
                 .padding()
                 .background(Color.black.opacity(0.1))
                 .cornerRadius(10)
-            }
-            Button("Fact") {
-                store.send(.factButtonTapped)
-            }
-            .font(.largeTitle)
-            .padding()
-            .background(Color.black.opacity(0.1))
-            .cornerRadius(10)
-            
-            if store.isLoading {
-                ProgressView()
-            } else if let fact = store.fact {
-                Text(fact)
-                    .font(.largeTitle)
-                    .multilineTextAlignment(.center)
-                    .padding()
-            
+                
+                Button(store.isTimerRunning ? "Stop timer" : "Start timer") {
+                  store.send(.toggleTimerButtonTapped)
+                }
+                .font(.largeTitle)
+                .padding()
+                .background(Color.black.opacity(0.1))
+                .cornerRadius(10)
+                
+                Button("Fact") {
+                    store.send(.factButtonTapped)
+                }
+                .font(.largeTitle)
+                .padding()
+                .background(Color.black.opacity(0.1))
+                .cornerRadius(10)
+                
+                if store.isLoading {
+                    ProgressView()
+                } else if let fact = store.fact {
+                    Text(fact)
+                        .font(.largeTitle)
+                        .multilineTextAlignment(.center)
+                        .padding()
+                    
+                }
             }
         }
     }
